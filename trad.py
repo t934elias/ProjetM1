1bb3471f25c070abc4be74a863e96c2d59c73411
Erwann-Gallois <gallois.erwann@gmail.com>
2024-12-03 18:06:18 +0100

    Refactor la fonction de traduction dans trad.py pour traiter plusieurs fichiers et améliorer la gestion des données

--- trad.py
+++ trad.py
@@ -2,18 +2,7 @@ from transformers import MarianMTModel, MarianTokenizer
 import pandas as pd
 import os
 
-data_path = os.path.join(os.path.dirname(__file__), "datasets/data/DAIC-WOZ")
-data_fr_path = os.path.join(os.path.dirname(__file__), "datasets/data_fr/DAIC-WOZ_FR")
-data = pd.read_csv(os.path.join(data_path, "300_TRANSCRIPT.csv"), sep="\t")
-
-texts = data["value"]
-# print(texts.isna().sum())
-
-data_fr = pd.DataFrame()
-data_fr = data.copy(deep=True)
-# data_fr['original'] = texts
-data_fr['trad'] = "Francais"
-# print(data_fr.head())
+# 1 : Psychiatrist, 2 : Patient
 
 # Nom du modèle pour la traduction EN->FR
 model_name = "Helsinki-NLP/opus-mt-en-fr"
@@ -22,14 +11,51 @@ model_name = "Helsinki-NLP/opus-mt-en-fr"
 tokenizer = MarianTokenizer.from_pretrained(model_name)
 model = MarianMTModel.from_pretrained(model_name)
 
-# Traduire les textes
-trad_tab = []
-for text in texts:
-    tokenized_text = tokenizer(text, return_tensors="pt", padding=True)
-    translated = model.generate(**tokenized_text)
-    trad_tab.append(tokenizer.decode(translated[0], skip_special_tokens=True))
-data_fr['trad'] = trad_tab
+def translate_DAIC():
+    data_path = os.path.join(os.path.dirname(__file__), "datasets/data/DAIC-WOZ")
+    data_fr_path = os.path.join(os.path.dirname(__file__), "datasets/data_fr/DAIC-WOZ_FR")
+    files = os.listdir(data_path)
+    # DAIC-WOZ
+    for file in files:
+        if len(files) % 10 == 0:
+            print(f"Traduction de {file} en cours... (DAIC-WOZ)")
+        data = pd.read_csv(os.path.join(data_path, file), sep="\t")
+        df_fr = pd.DataFrame(columns=["speaker", "original", "traduit"])
+        for line in data.iterrows():
+            speaker = line[1]["speaker"]
+            if speaker == "Ellie":
+                speaker = 1
+            else:
+                speaker = 2
+            dialog = line[1]["value"]
+            tokenized_text = tokenizer(dialog, return_tensors="pt", padding=True)
+            translated = model.generate(**tokenized_text)
+            tempo = pd.DataFrame(columns=["speaker", "original", "traduit"], data=[[speaker, dialog, tokenizer.decode(translated[0], skip_special_tokens=True)]])
+            df_fr = pd.concat([df_fr, tempo], ignore_index=True)
+        df_fr.to_csv(os.path.join(data_fr_path, file + "_FR.csv"), sep="\t", index=False)
+    print("Dossier DAIC-WOZ traduit avec succès !")
 
-# Sauvegarder les traductions
-data_fr.to_csv(os.path.join(data_fr_path, "300_TRANSCRIPT_FR.csv"), sep="\t", index=False)
-print("Traduction terminée")
+# DAMT
+def translate_DAMT():
+    data_path = os.path.join(os.path.dirname(__file__), "datasets/data/DAMT/transcripts/transcribed")
+    files = os.listdir(data_path)
+    data_fr_path = os.path.join(os.path.dirname(__file__), "datasets/data_fr/DAMT_FR")
+    for file in files:
+        if len(files) % 10 == 0:
+            print(f"Traduction de {file} en cours... (DAMT)")
+        data = pd.read_json(os.path.join(data_path, file))
+        df_fr = pd.DataFrame(columns=["speaker", "original", "traduit"])
+        for line in data.iterrows():
+            speaker = line[1]["speaker"]
+            dialog = line[1]["dialogue"]
+            for sent in dialog:
+                tokenized_text = tokenizer(sent, return_tensors="pt", padding=True)
+                translated = model.generate(**tokenized_text)     
+                tempo = pd.DataFrame(columns=["speaker", "original", "traduit"], data=[[speaker, sent, tokenizer.decode(translated[0], skip_special_tokens=True)]])
+                df_fr = pd.concat([df_fr, tempo], ignore_index=True)
+        df_fr.to_csv(os.path.join(data_fr_path, file + "_FR.csv"), sep="\t", index=False)
+    print("Le dossier DAMT traduit avec succès !")
+
+translate_DAIC()
+translate_DAMT()
+print("Traduction terminée !")
